ba7becf8fb62bab6d5881b4bdc369b85405d5243
acaf02 <anacarol.alves1@outlook.com>
2024-12-09 13:06:24 -0300

    feat: adicionar importação do arquivo Organizations

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Publications from "./components/Publications";
+import Organizations from "./components/Organizations";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         <Experience />
         <Projects />
         <Publications />
+        <Organizations />
         <Contact />
       </div>
     </div>
