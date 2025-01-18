b346f76edb44b77789cdce7ac524671919104541
acaf02 <anacarol.alves1@outlook.com>
2025-01-18 15:03:59 -0300

    feat: colocar novas informações

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Technologies from "./components/Technologies";
+import Academy from "./components/Academy";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -17,6 +18,7 @@ const App = () => {
         <Hero />
         <About />
         <Technologies />
+        <Academy />
         <Experience />
         <Projects />
         <Publications />
