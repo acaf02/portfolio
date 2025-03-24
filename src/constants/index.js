f969f1f40f5fda8e9d5066c3c5b94d5b59463232
Ana <122612165+acaf02@users.noreply.github.com>
2025-03-24 13:53:12 -0300

    feat: adicionar novas informações

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,14 +4,14 @@ import project3 from "../assets/projects/project3.jpg";
 
 export const HERO_CONTENT = `Sou uma desenvolvedora apaixonada por criar aplicações web robustas e escaláveis. Com experiência prática acumulada durante minha formação, tenho aprimorado minhas habilidades em tecnologias de front-end, como React, HTML, CSS, JS, Bootstrap e back-end, incluindo Node.js, PHP, Java, PostgreSQL e MySQL. Meu objetivo é utilizar minha expertise para criar soluções inovadoras que impulsionem o crescimento dos negócios e ofereçam experiências excepcionais aos usuários.`;
 
-export const ABOUT_TEXT = `Sou estudante do curso de Análise e Desenvolvimento de Sistemas no Instituto Federal Farroupilha - Campus Alegrete, atualmente no 6º semestre. Com experiência em diversas tecnologias, como Java, Node.js, JavaScript, HTML, CSS, PHP e Bootstrap, desenvolvi uma base sólida tanto no front-end quanto no back-end. Também tenho conhecimento em bancos de dados PostgreSQL e MySQL, além de metodologias ágeis. Recentemente, tenho me aprofundado em React e Spring Boot, com o objetivo de explorar novas possibilidades no desenvolvimento de software e aprimorar minhas habilidades técnicas. Fora dos estudos, gosto de ler, assistir séries e filmes, e explorar novas tecnologias.`;
+export const ABOUT_TEXT = `Formada em Análise e Desenvolvimento de Sistemas pelo Instituto Federal Farroupilha - Campus Alegrete, atualmente fazendo pós graduação de Tecnologias e Inovações Web na instituição FAVENI. Com experiência em diversas tecnologias, como Java, Node.js, JavaScript, HTML, CSS, PHP e Bootstrap, desenvolvi uma base sólida tanto no front-end quanto no back-end. Também tenho conhecimento em bancos de dados PostgreSQL e MySQL, além de metodologias ágeis. Recentemente, tenho me aprofundado em React e Spring Boot, com o objetivo de explorar novas possibilidades no desenvolvimento de software e aprimorar minhas habilidades técnicas. Fora dos estudos, gosto de ler, assistir séries e filmes, e explorar novas tecnologias.`;
 
 export const EXPERIENCES = [
   {
     year: "Dezembro/2024 - até o momento",
     role: "Estágio",
-    company: "FRANCISCO DAS CHAGAS FERREIRA DE OLIVEIRA JUNIOR",
-    description: `Desenvolvimento de um sistema web para a loja Lux Revestimentos`,
+    company: "Cartório Serviço de Registro Civil das Pessoas Naturais e Registros Especiais",
+    description: `Desenvolvimento de um sistema web para a loja Lux Revestimentos e Acabamentos`,
     technologies: [
       "React.js",
       "React Router",
@@ -102,6 +102,22 @@ export const PUBLICATION = [
         "Projeto interdisciplinar no curso de Análise e Desenvolvimento de Sistemas (ADS), focado no desenvolvimento de um aplicativo para registro e acompanhamento dos Boletins de Atendimento da Guarda Municipal de Alegrete. O projeto integrou disciplinas como Engenharia de Software, Banco de Dados, Programação e Interface Humano-Computador (IHC), abordando todas as fases do ciclo de vida do desenvolvimento de software. " +
         "Minha atuação incluiu a coleta de requisitos, modelagem UML, desenvolvimento de banco de dados em PostgreSQL e criação de histórias e épicos para o gerenciamento de funcionalidades. Trabalhei em colaboração com representantes da Guarda Municipal e colegas de turma para refinar e aprimorar o aplicativo, garantindo que atendesse às necessidades específicas da corporação.",
     },
+  {
+      title: "StockManager: um software para controle de estoques alimentícios no refeitório do IFFAR - Campus Alegrete",
+      publication: "XV Mostra da Educação Profissional e Tecnológica",
+      url: "https://arandu.iffarroupilha.edu.br/handle/itemid/540",
+      description:
+        "Página: 788" +
+        "Resumo do artigo: Os estoques desempenham um papel importante na gestão empresarial, com o gerenciamento eficaz dos estoques ajuda a evitar o excesso e a escassez de produtos, assim contribuindo com a otimização do investimento e maximizando a eficiência dos meios internos da empresa. A partir desse contexto, esse trabalho propõe o desenvolvimento de um software personalizado para o controle dos insumos alimentícios do refeitório Instituto Federal Farroupilha - Campus Alegrete, visando aprimorar e otimizar o controle e a administração de todos os produtos disponíveis. Para alcançar esses objetivos, utilizou-se a metodologia de desenvolvimento de software baseada no modelo em cascata, assegurando que cada etapa seja concluída antes do início da próxima.",
+    },
+  {
+      title: "CHEMICALL: Sistema de gestão de produtos químicos para laboratórios de Institutos Federais, um estudo de caso IFFarroupilha",
+      publication: "XV Mostra da Educação Profissional e Tecnológica",
+      url: "https://arandu.iffarroupilha.edu.br/handle/itemid/540",
+      description:
+        "Página: 785" +
+        "Resumo do artigo: A gestão eficiente de produtos químicos em laboratórios é essencial para garantir a segurança, o controle e a utilização adequada desses materiais. Nesse contexto, este trabalho tem como objetivo desenvolver um software de gestão de estoque de produtos químicos específico para os Laboratórios de Química do Instituto Federal Farroupilha. Este software será desenvolvido baseado no método em cascata de Sommerville e com a realização desse trabalho espera-se otimizar o registro e remoção de produtos químicos do estoque, exibir o status de utilização dos produtos químicos já existentes na instituição e expedir um relatório com o status e estoque de todos os produtos disponíveis atualmente.",
+    },
   ];
   
 
@@ -109,17 +125,17 @@ export const ORGANIZATION = [
   {
   title: "Diretório Acadêmico Ada Augusta Byron King",
   role: "Tesoureira e Diretora de Comunicações",
-  period: "Junho de 2024 - até o momento",
+  period: "Junho de 2024 - Fevereiro de 2025",
   description: `
-        Atualmente, sou Tesoureira e Diretora de Comunicação, além de membro ativo do Diretório Acadêmico do curso de Análise e Desenvolvimento de Sistemas do Instituto Federal Farroupilha (IFFAR). Minhas responsabilidades incluem:
+Fui Tesoureira e Diretora de Comunicação do Diretório Acadêmico do curso de Análise e Desenvolvimento de Sistemas do Instituto Federal Farroupilha (IFFAR). Minhas responsabilidades incluíram:
 
-        - Gestão financeira: Como tesoureira, sou responsável pelo controle financeiro das atividades do Diretório Acadêmico, gerenciando os recursos de maneira eficiente para garantir a realização de eventos e iniciativas que beneficiem os alunos.
+- Gestão financeira: Como tesoureira, fui responsável pelo controle financeiro das atividades do Diretório Acadêmico, gerenciando os recursos de maneira eficiente para garantir a realização de eventos e iniciativas que beneficiem os alunos.
 
-        - Comunicação e promoção do curso: No papel de Diretora de Comunicação, lidero a criação e execução de estratégias de comunicação para divulgar as atividades do Diretório Acadêmico, promover o curso e fortalecer a identidade do grupo. Isso inclui a gestão de redes sociais, elaboração de materiais de divulgação e organização de campanhas que ampliem o alcance das nossas iniciativas.
+- Comunicação e promoção do curso: No papel de Diretora de Comunicação, liderei a criação e execução de estratégias de comunicação para divulgar as atividades do Diretório Acadêmico, promover o curso e fortalecer a identidade do grupo. Isso inclui a gestão de redes sociais, elaboração de materiais de divulgação e organização de campanhas que ampliem o alcance das nossas iniciativas.
 
-        - Planejamento e organização de eventos acadêmicos: Contribuo ativamente para o planejamento e a realização de eventos como a Semana Acadêmica, workshops e palestras. Meu trabalho envolve tanto o suporte logístico quanto a comunicação com palestrantes, parceiros e participantes, visando o sucesso dos eventos e o engajamento da comunidade acadêmica.
+- Planejamento e organização de eventos acadêmicos: Contribui ativamente para o planejamento e a realização de eventos como a Semana Acadêmica, workshops e palestras. Meu trabalho envolveu tanto o suporte logístico quanto a comunicação com palestrantes, parceiros e participantes, visando o sucesso dos eventos e o engajamento da comunidade acadêmica.
 
-        - Apoio aos estudantes: Facilito a comunicação entre os alunos e a coordenação do curso, atuando como um canal de diálogo para identificar e atender às necessidades dos estudantes. Também colaboro na criação de iniciativas que melhorem a experiência acadêmica e promovam o desenvolvimento pessoal e profissional dos alunos.
+- Apoio aos estudantes: Facilitei a comunicação entre os alunos e a coordenação do curso, atuando como um canal de diálogo para identificar e atender às necessidades dos estudantes. Também colaborei na criação de iniciativas que melhorem a experiência acadêmica e promovam o desenvolvimento pessoal e profissional dos alunos.
 `,
 },
 ];
