79e16b5ecef5461b2b99aafb24054fa24a5674bc
Ana <122612165+acaf02@users.noreply.github.com>
2025-05-08 09:37:09 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/portfolio/',
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
 });
